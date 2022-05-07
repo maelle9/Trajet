3e02eed2300bfe7333b635ea334f01140303d19a
Kamomille <camille.bayon-de-noyer@esme.fr>
2022-05-07 21:52:28 +0200

    update

--- main.js
+++ main.js
@@ -3,33 +3,14 @@ import * as F from "./fonctions.js";
 import * as C from "./cities.js";
 
 
-export const Distance = cities => (from, to) => R.pipe(
-    R.filter(R.propEq('from', from)),
-    R.find(R.propEq('to', to)),
-    R.path(['km']),
-)(C.citiesList);
 
-export const SelectOneCity = cities => (city) => R.pipe(
-    R.filter(R.propEq('from', city))
-)(C.citiesList);
+console.log(F.Distance(C.citiesList)('paris','marseille'));
 
-export const SelectOneCity_FindMin = cities => (city) => R.pipe(
-    R.filter(R.propEq('from', city)),
-    R.sortBy(R.prop('km')),
-    R.head()
-)(C.citiesList);
+console.log(F.SelectOneCity(C.citiesList)('paris'));
 
-export const Delete = cities => (from, to) => R.pipe(
-    R.reject (R.where ({from: R.includes (from), to: R.includes (to)}))
-)(C.citiesList);
+console.log(F.SelectOneCity_FindMin(C.citiesList)('paris'));
 
-console.log(Distance(C.citiesList)('paris','marseille'));
-
-console.log(SelectOneCity(C.citiesList)('paris'));
-
-console.log(SelectOneCity_FindMin(C.citiesList)('paris'));
-
-console.log(Delete(C.citiesList)('paris','marseille'));
+console.log(F.Delete(C.citiesList)('paris','marseille'));
 
 
 
