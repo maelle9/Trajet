07f635eb078b78c8410309fb99b176fc4a078b38
Kamomille <camille.bayon-de-noyer@esme.fr>
2022-05-08 19:12:44 +0200

    update

--- fonctions.js
+++ fonctions.js
@@ -1,27 +1,71 @@
 import * as R from "ramda";
 import * as C from "./cities.js";
 
+// ====================================================================
+//                  Fonctions de base
+// ====================================================================
+
 // Permet de trouver la distance en km entre 2 villes données
-export const Distance = cities => (from, to) => R.pipe(
+export const Distance = citiesList => (from, to) => R.pipe(
     R.filter(R.propEq('from', from)),
     R.find(R.propEq('to', to)),
     R.path(['km']),
-)(C.citiesList);
+)(citiesList);
 
-// Selectionne tous les possibilités de trajets pour 1 ville donnée
-export const SelectOneCity = cities => (city) => R.pipe(
+// Selectionne toutes les possibilités de trajets pour 1 ville donnée
+export const SelectOneCity = citiesList => (city) => R.pipe(
     R.filter(R.propEq('from', city))
-)(C.citiesList);
+)(citiesList);
 
-// Selectionne la ville la plus proche
-export const VilleLaPlusProche = cities => (from) => R.pipe(
+// Selectionne la ville la plus proche de la ville choisi
+export const VilleLaPlusProche = citiesList => (from) => R.pipe(
     R.filter(R.propEq('from', from)),
     R.sortBy(R.prop('km')),
     R.head(),
     R.path(['to']),
-)(C.citiesList);
+)(citiesList);
 
-export const Delete = cities => (city_1, city_2) => R.pipe(
+export const DistanceVilleLaPlusProche = citiesList => (from) => R.pipe(
+    R.filter(R.propEq('from', from)),
+    R.sortBy(R.prop('km')),
+    R.head(),
+    R.path(['km']),
+)(citiesList);
+
+// Supprime le trajet entre 2 villes de citiesList pour ne pas le refaire
+export const Delete = citiesList => (city_1, city_2) => R.pipe(
     R.reject (R.where ({from: R.includes (city_1), to: R.includes (city_2)})),
     R.reject (R.where ({from: R.includes (city_2), to: R.includes (city_1)}))
-)(C.citiesList);
+)(citiesList);
+
+
+// ====================================================================
+//      Détermine la ville suivante à ajouter dans l'arbre couvrant
+// ====================================================================
+
+export const ListBestOption = (items, newCitiesList) => {
+    const copyItems = [];
+    R.forEach((item) => {
+        copyItems.push({from : item, to : VilleLaPlusProche(newCitiesList)(item), km : DistanceVilleLaPlusProche(newCitiesList)(item)});
+    })(items);
+    return copyItems
+}
+export const SelectBestOption = copyItems => () => R.pipe(
+    R.sortBy(R.prop('km')),
+    R.head(),
+    R.path(['to']),
+)(copyItems);
+
+export const GetNameCityInArbreCouvrant = arbreCouvrant => () => R.pipe(
+    R.pluck('city'),
+)(arbreCouvrant);
+
+// ====================================================================
+//      Fonctions pour l'arbre couvrant
+// ====================================================================
+
+// Ajoute une nouvelle ville et son enfant dans l'arbre couvrant
+export const AddNewCityInArbreCouvrant = arbreCouvrant => (city, nameChild) => R.pipe(
+    R.append({city :city, child:[nameChild]})
+)(arbreCouvrant);
+
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as R from "ramda";
 import * as F from "./fonctions.js";
 import * as C from "./cities.js";
 
-
+/*
 console.log(F.Distance(C.citiesList)('paris','marseille'));
 
 console.log(F.SelectOneCity(C.citiesList)('paris'));
@@ -11,24 +11,62 @@ console.log(F.VilleLaPlusProche(C.citiesList)('paris'));
 
 console.log(F.Delete(C.citiesList)('paris','marseille'));
 
+console.log(GetNameCityInArbreCouvrant(arbreCouvrant)());
+
+//console.log(F.AddNewCityInArbreCouvrant(arbreCouvrant_initialisation)('paris','lyon'));
+ */
+
+
 
 // ----------- arbre couvrant construction ----------- // PAS FINI
 
 console.log('--- Arbre couvrant ---');
 
+const nombreDeVille = R.length(F.SelectOneCity(C.citiesList)('paris'));
+
 const nomVilleDepart = 'paris'
 
-const arbreCouvrant = [{city:nomVilleDepart, child:[F.VilleLaPlusProche(C.citiesList)(nomVilleDepart)]},
-    {city:F.VilleLaPlusProche(C.citiesList)(nomVilleDepart), child:[]}];
+//let arbreCouvrant = F.AddNewCityInArbreCouvrant([])(nomVilleDepart,F.VilleLaPlusProche(C.citiesList)(nomVilleDepart,C.citiesList));
+let arbreCouvrant = [
+    { city: 'paris', child: [ 'lyon' ] },
+    { city: 'lyon', child: [] },];
 
-console.log(F.Delete(C.citiesList)(nomVilleDepart, F.VilleLaPlusProche(C.citiesList)(nomVilleDepart)));
+//let newCitiesList = F.Delete(C.citiesList)(nomVilleDepart, F.VilleLaPlusProche(C.citiesList)(nomVilleDepart,C.citiesList));
+let newCitiesList = F.Delete(C.citiesList)('paris', 'lyon');
+
+
+const ListNameCityInArbreCouvrant = F.GetNameCityInArbreCouvrant(arbreCouvrant)();
+console.log(ListNameCityInArbreCouvrant)
+
+
+const nextCityToPutInTree = F.SelectBestOption(F.ListBestOption (ListNameCityInArbreCouvrant, newCitiesList))();
+
+console.log(nextCityToPutInTree)
+
+
+
+
+//console.log(arbreCouvrant)
+//console.log(newCitiesList)
+
+let v1, v2;
+
+
+/*
+const test = (list) =>
+    R.pipe(
+        R.sort(),
+        R.head()
+    );
+for (let i = 0 ; i < nombreDeVille -1 ; i++){
+
+    //F.DisplayListDistanceMin(newCitiesList,arbreCouvrant, nomVilleDepart);
+    v1 = test(F.DisplayListDistanceMin(newCitiesList,arbreCouvrant, nomVilleDepart));
+    console.log(v1);
+    v2 = F.VilleLaPlusProche(newCitiesList)(nomVilleDepart,newCitiesList);
+    arbreCouvrant = F.AddNewCityInArbreCouvrant(arbreCouvrant)(v1,v2);
+
+    newCitiesList = F.Delete(newCitiesList)(v1, v2);
+}
 console.log(arbreCouvrant)
-
-
-// ----------- test -----------
-const getMetrics = R.applySpec({
-    city: R.add,
-    child: [R.multiply]
-});
-console.log(getMetrics(2, 4)); // => { sum: 6, nested: { mul: 8 } }
-
+*/
